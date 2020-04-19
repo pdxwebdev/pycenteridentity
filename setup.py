001e1b243ef346570674cf9be31f7dadb2181b36
Matt Vogel <matt@portlandwebdev.com>
2020-04-19 01:21:25 -0700

    update requirements

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from distutils.core import setup
 setup(
     name='centeridentity',         # How you named your package folder (MyLib)
     packages=['centeridentity'],   # Chose the same as "name"
-    version='0.11',      # Start with a small number and increase it with every change you make
+    version='0.12',      # Start with a small number and increase it with every change you make
     license='MIT',        # Chose a license from here: https://help.github.com/articles/licensing-a-repository
     description='API wrapper for center identity',   # Give a short description about your library
     long_description="Center Identity API wrapper",
@@ -16,7 +16,7 @@ setup(
     keywords=['blockchain', 'identity'],   # Keywords that define your package best
     install_requires=[            # I get to this in a second
         'requests',
-        'eccsnacks @ git+https://github.com/pdxwebdev/eccsnacks',
+        'eccsnacks',
         'coincurve',
         'mnemonic',
         'bip32utils',
